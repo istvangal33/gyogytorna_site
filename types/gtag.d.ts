5a983f670108f3254c70902f0ba8872068c20ec4
istvangal33 <145024375+istvangal33@users.noreply.github.com>
2025-10-17 17:32:43 +0200

    twenty-sixth commit

---
+++ types/gtag.d.ts
@@ -0,0 +1,12 @@
+export {};
+
+declare global {
+  interface Window {
+    gtag: (
+      command: 'config' | 'event' | 'consent',
+      targetId: string,
+      config?: Record<string, any>
+    ) => void;
+    dataLayer: any[];
+  }
+}
